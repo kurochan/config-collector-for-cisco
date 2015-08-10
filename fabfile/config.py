560473a1d83f94c4e44fe4d1cb76030df70fed21
kurochan <kuro@kurochan.org>
2015-08-11 00:28:28 +0900

    ファイル出力機能追加

--- fabfile/config.py
+++ fabfile/config.py
@@ -15,10 +15,19 @@ class CollectConfig(BaseTask):
   def run_task(self, *args, **kwargs):
     host_config = env.inventory.get_variables(env.host)
     config = self.get_config(host_config['ssh_host'], host_config['ssh_user'], host_config['ssh_pass'], host_config['exec_pass'])
+    self.write_config(env.host, config)
     # print config
 
   def get_config(self, hostname, ssh_user, ssh_pass, exec_pass):
     script_name = "dump-config-cisco-ios.sh"
-    local(os.path.dirname(os.path.abspath(__file__)) + "/../bin/{0} {1} {2} {3}".format(script_name, ssh_user, hostname, ssh_pass))
+    config = local(os.path.dirname(os.path.abspath(__file__)) + "/../bin/{0} {1} {2} {3}".format(script_name, ssh_user, hostname, ssh_pass), capture = True)
+    return config
+
+  def write_config(self, hostname, config):
+    output_dir = os.path.dirname(os.path.abspath(__file__)) + "/../tmp/config"
+    local("mkdir -p {0}".format(output_dir))
+    file = open("{0}/{1}.txt".format(output_dir, hostname), 'w')
+    file.write(str(config))
+    file.close()
 
 collect = CollectConfig()
