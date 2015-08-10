176a90a5898e1bc17bf939a34ab2a369b5c67b7c
kurochan <kuro@kurochan.org>
2015-08-11 00:11:41 +0900

    refactor

--- fabfile/config.py
+++ fabfile/config.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from fabric.api import *
 from fabric.state import output
 from fabric.colors import *
@@ -7,16 +8,17 @@ from base import BaseTask
 from helper.print_helper import task_puts
 
 class CollectConfig(BaseTask):
-    """
-    collect configuration
-    """
-    name = "collect"
-    def run_task(self, *args, **kwargs):
-      host_config = env.inventory.get_variables(env.host)
-      config = self.get_config(host_config['ssh_host'], host_config['ssh_user'], host_config['ssh_pass'], host_config['exec_pass'])
-      # print config
+  """
+  collect configuration
+  """
+  name = "collect"
+  def run_task(self, *args, **kwargs):
+    host_config = env.inventory.get_variables(env.host)
+    config = self.get_config(host_config['ssh_host'], host_config['ssh_user'], host_config['ssh_pass'], host_config['exec_pass'])
+    # print config
 
-    def get_config(self, hostname, ssh_user, ssh_pass, exec_pass):
-      local("bin/dump-config-cisco-csr.sh {0} {1} {2}".format(ssh_user, hostname, ssh_pass))
+  def get_config(self, hostname, ssh_user, ssh_pass, exec_pass):
+    script_name = "dump-config-cisco-ios.sh"
+    local(os.path.dirname(os.path.abspath(__file__)) + "/../bin/{0} {1} {2} {3}".format(script_name, ssh_user, hostname, ssh_pass))
 
 collect = CollectConfig()
