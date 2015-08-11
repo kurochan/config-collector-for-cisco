130ed48d47e62e73422bda312b4941ab9c57e47b
kurochan <kuro@kurochan.org>
2015-08-11 19:22:37 +0900

    refactor

--- fabfile/git.py
+++ fabfile/git.py
@@ -19,14 +19,19 @@ class PushGit(BaseTask):
 
     if self.git_repo_exists(output_dir):
       with lcd(output_dir):
-        with settings(warn_only=True):
+        with quiet():
+          result = local("git diff --exit-code")
+        if not result.failed:
+          task_puts("nothing to commit")
+        else:
           task_puts("commit config")
           local("git add -A")
           local("git commit -m \"{0}\"".format(datetime.now().strftime("%Y/%m/%d %H:%M:%S")))
+          local("git pull --rebase origin master")
           task_puts("push config")
           local("git push origin master")
     else:
-      task_puts("Nothing to push")
+      raise Exception("no git directory")
 
   def git_repo_exists(self, base_path):
     return os.path.exists("{0}/.git".format(base_path))
